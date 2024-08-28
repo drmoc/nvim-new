8f477c580c200f62c10675150c1ea866d300316e
drmoc <mocarvalho@outlook.com>
2024-08-28 03:25:59 -0300

    commit

--- lua/user/plugins/init.lua
+++ lua/user/plugins/init.lua
@@ -4,4 +4,5 @@ return {
   "christoomey/vim-tmux-navigator", -- tmux & split window navigation
 
   "inkarkat/vim-ReplaceWithRegister", -- replace with register contents using motion (gr + motion)
+
 }
