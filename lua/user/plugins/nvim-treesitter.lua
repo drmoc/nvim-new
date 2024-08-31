a6f29d56a3c869459ca44d82d334a13816ce72f7
drmoc <mocarvalho@outlook.com>
2024-08-31 08:25:00 -0300

    commit

--- lua/user/plugins/nvim-treesitter.lua
+++ lua/user/plugins/nvim-treesitter.lua
@@ -1,74 +1,73 @@
 return {
-	{
-		"nvim-treesitter/nvim-treesitter",
-		event = { "BufReadPre", "BufNewFile" },
-		build = ":TSUpdate",
-		dependencies = {
-			"nvim-treesitter/nvim-treesitter-textobjects",
-			"windwp/nvim-ts-autotag",
-		},
-		config = function()
-			-- import nvim-treesitter plugin
-			local treesitter = require("nvim-treesitter.configs")
+  {
+    "nvim-treesitter/nvim-treesitter",
+    event = { "BufReadPre", "BufNewFile" },
+    build = ":TSUpdate",
+    dependencies = {
+      "nvim-treesitter/nvim-treesitter-textobjects",
+      "windwp/nvim-ts-autotag",
+    },
+    config = function()
+      -- import nvim-treesitter plugin
+      local treesitter = require("nvim-treesitter.configs")
 
-			-- configure treesitter
-			treesitter.setup({
-				-- enable syntax highlighting
-				highlight = {
-					enable = true,
-				},
-				-- Enable delimiter highlighting
-				rainbow = {
-					enable = true,
-					extended_mode = true, -- Também colore tags HTML
-					max_file_lines = nil, -- Não desative para arquivos grandes
-				},
-				-- enable indentation
-				indent = { enable = true },
-				-- enable autotagging (w/ nvim-ts-autotag plugin)
-				autotag = {
-					enable = true,
-				},
-				-- ensure these language parsers are installed
-				ensure_installed = {
-					"json",
-					"javascript",
-					"typescript",
-					"tsx",
-					"yaml",
-					"html",
-					"css",
-					"prisma",
-					"markdown",
-					"markdown_inline",
-					"svelte",
-					"graphql",
-					"bash",
-					"lua",
-					"vim",
-					"dockerfile",
-					"gitignore",
-					"query",
-					"norg",
-				},
-				incremental_selection = {
-					enable = true,
-					keymaps = {
-						init_selection = "<C-space>",
-						node_incremental = "<C-space>",
-						scope_incremental = false,
-						node_decremental = "<bs>",
-					},
-				},
-				-- Remove a seção context_commentstring existente
-				context_commentstring = nil,
+      -- configure treesitter
+      treesitter.setup({
+        -- enable syntax highlighting
+        highlight = {
+          enable = true,
+        },
+        -- Enable delimiter highlighting
+        rainbow = {
+          enable = true,
+          extended_mode = true, -- Também colore tags HTML
+          max_file_lines = nil, -- Não desative para arquivos grandes
+        },
+        -- enable indentation
+        indent = { enable = true },
+        -- enable autotagging (w/ nvim-ts-autotag plugin)
+        autotag = {
+          enable = true,
+        },
+        -- ensure these language parsers are installed
+        ensure_installed = {
+          "json",
+          "javascript",
+          "typescript",
+          "tsx",
+          "yaml",
+          "html",
+          "css",
+          "prisma",
+          "markdown",
+          "markdown_inline",
+          "svelte",
+          "graphql",
+          "bash",
+          "lua",
+          "vim",
+          "dockerfile",
+          "gitignore",
+          "query",
+          "norg",
+        },
+        incremental_selection = {
+          enable = true,
+          keymaps = {
+            init_selection = "<C-space>",
+            node_incremental = "<C-space>",
+            scope_incremental = false,
+            node_decremental = "<bs>",
+          },
+        },
+        -- Remove a seção context_commentstring existente
+        context_commentstring = nil,
 
-				-- Adicione a nova configuração para ts_context_commentstring
-				require("ts_context_commentstring").setup({}),
+        -- Adicione a nova configuração para ts_context_commentstring
+        require("ts_context_commentstring").setup({}),
 
-				-- Adicione a configuração skip_ts_context_commentstring_module para acelerar o carregamento
-				skip_ts_context_commentstring_module = true,
-			})
-		end,
-	},
+        -- Adicione a configuração skip_ts_context_commentstring_module para acelerar o carregamento
+        })
+    end,
+  },
 }
