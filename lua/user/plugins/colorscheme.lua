181f7a4aac0173047b1a1b8c26650f7158cfd9b1
drmoc <mocarvalho@outlook.com>
2024-09-18 09:50:03 -0300

    commit4

--- lua/user/plugins/colorscheme.lua
+++ lua/user/plugins/colorscheme.lua
@@ -3,7 +3,7 @@ return {
 	name = "catppuccin",
 	priority = 1000,
 	config = function()
-		require("catppccin").setup({
+		require("catppuccin").setup({
 			flavour = "auto", -- latte, frappe, macchiato, mocha
 			background = { -- :h background
 				light = "latte",
